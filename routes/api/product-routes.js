07ff1e554251508e09bdf727f9e350d488b2ec5b
Olubukola Abiona <bukyabiona@gmail.com>
2023-02-03 01:53:51 -0400

    got the delete request for product route to work

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -20,9 +20,22 @@ router.get('/', async(req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  try{
+    const productsData = await Product.findByPk(req.params.id, {
+      // be sure to include its associated Category and Tag data
+      include: [{model: Category}, {model: Tag}]
+    });
+    if(!productsData){
+      res.status(404).json({message: 'No product found with that id'});
+    return;
+    }
+    res.status(200).json(productsData);
+  }catch(err){
+    res.status(500).json(err);
+  }
+  
 });
 
 // create new product
@@ -100,6 +113,21 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(productData => {
+    if(!productData){
+      res.status(404).json({message: "No product found with that id"});
+      return;
+    }
+    res.status(200).json(productData);
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  })
   // delete one product by its `id` value
 });
 
